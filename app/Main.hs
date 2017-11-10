b8c67920787901bd16aadf666088afd0c2603259
Sencho Pens <senya@chekanov.net>
2017-11-10 16:46:36 +0300

    style: Align commands

--- app/Main.hs
+++ app/Main.hs
@@ -28,14 +28,14 @@ runNode :: NodeConfig -> Flowers -> Process ()  -- функция выполне
 runNode config@(NodeConfig repl) flowers = do
   let run = runNode config
   receiveWait  -- ждем сообщений
-    [ match (\command ->  -- если нам пришло что-то типа Command от REPL
+    [ match (\command ->  -- если нам пришло что-то типа Command от REPL:
         case command of
           (Add flower) -> do  -- команда добавления элемента от пользователя
-            send repl (Added flower)  -- отправить REPLу сообщение, что цветок добавлен
+            send repl (Added flower)    -- отправить REPLу сообщение, что цветок добавлен
             run $ S.add flower flowers  -- запускаем ее уже с новым цветком
           Show -> do  -- запрос показать цветочки
             send repl (HereUR $ toList flowers)  -- отправить цветочки в виде списка
-            run flowers)  -- ничего не поменялось
+            run flowers)
 
     , match (\Tick -> do  -- сигнал о том, что надо поделиться своим состоянием с другими
         P2P.nsendPeers "bees" flowers  -- отправить всем пирам цветки
@@ -47,20 +47,20 @@ runNode config@(NodeConfig repl) flowers = do
   
 spawnNode :: Process ()
 spawnNode = do
-  liftIO $ threadDelay 3000000  -- дать bootstrap ноде время для запуска
-  let flowers = S.initial :: Flowers  -- инициализирум GSet координат цветков
-  self <- getSelfPid  -- получаем наш Pid чтобы REPL мог посылать нам сообщения
+  liftIO $ threadDelay 3000000       -- дать bootstrap ноде время для запуска
+  let flowers = S.initial :: Flowers -- инициализирум GSet координат цветков
+  self <- getSelfPid                 -- получаем наш Pid чтобы REPL мог посылать нам сообщения
   repl <- spawnLocal $ runRepl self  -- создаем REPL в отдельном потоке
-  register "bees" self  -- теперь нода будет получать сообщения из канала "bees"
+  register "bees" self               -- теперь нода будет получать сообщения из канала "bees"
   spawnLocal $ forever $ do  -- запускаем тикер:
-    send self Tick  -- оповестить основной поток что надо передать пирам свое состояние
+    send self Tick               -- оповестить основной поток что надо передать пирам свое состояние
     liftIO $ threadDelay $ 10^6  -- ждемс 0.1 секунды перед тем, как снова отослать состояние
-  runNode (NodeConfig repl) flowers
+  runNode (NodeConfig repl) flowers  -- запускаем ноду
 
-main = do  -- создаем монаду типа IO (), где будем совершать все действия
+main = do
   [port, bootstrapPort] <- getArgs  -- считываем порт ноды и bootstrap ноды
-  P2P.bootstrap  -- функция инициализации ноды 
+  P2P.bootstrap                     -- функция инициализации ноды 
     "127.0.0.1" port  -- IP и порт ноды
     [P2P.makeNodeId ("127.0.0.1:" ++ bootstrapPort)]  -- список bootstrap нод 
     initRemoteTable  -- создаем remote table
-    spawnNode  -- передаем функцию запуска ноды, ее код мы напишем потом
+    spawnNode        -- передаем функцию запуска ноды, ее код мы напишем потом
